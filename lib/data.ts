65d79fa95441e1b0434cad38eda05eb0da93621d
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-18 23:47:46 +0530

    feat: Add HeaderNav for navigation links

---
+++ lib/data.ts
@@ -0,0 +1,22 @@
+export const HeaderNav = [
+  {
+    title: "About",
+    link: "#about",
+  },
+  {
+    title: "Timeline",
+    link: "#timeline",
+  },
+  {
+    title: "Prizes",
+    link: "#prizes",
+  },
+  {
+    title: "Sponsors",
+    link: "#sponsors",
+  },
+  {
+    title: "FAQ",
+    link: "#faq",
+  },
+];
